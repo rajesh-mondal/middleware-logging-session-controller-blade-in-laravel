a2d7a1b192e8d6d4383725be59f1257f94a547d4
Rajesh Mondal <rajesh01749@gmail.com>
2025-07-23 15:56:15 +0600

    handle request headers within custom middleware

---
+++ app/Http/Controllers/DemoController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class DemoController extends Controller {
+    // Request Verfication
+    function RequestVerification(): string {
+        return "Hello";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\DemoController;
 use App\Http\Controllers\LogController;
 use App\Http\Controllers\SessionController;
+use App\Http\Middleware\DemoMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get( '/log/{num1}/{num2}', [LogController::class, 'LogAction'] );
@@ -11,3 +13,6 @@ Route::get( '/SessionPull', [SessionController::class, 'SessionPull'] );
 Route::get( '/SessionGet', [SessionController::class, 'SessionGet'] );
 Route::get( '/SessionForget', [SessionController::class, 'SessionForget'] );
 Route::get( '/SessionFlush', [SessionController::class, 'SessionFlush'] );
+
+// Request Verification
+Route::get( "hello", [DemoController::class, 'RequestVerification'] )->middleware( [DemoMiddleware::class] );
